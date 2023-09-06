9c954259d2ff99c2ed53c2a6acb909ac031835f2
stiproot <stipcich.simon@gmail.com>
2023-09-06 16:19:30 +0200

    feat:couchbase

---
+++ couchbase/init-create-bucket.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+/opt/couchbase/bin/couchbase-cli bucket-create \
+	-c localhost \
+	-u root \
+	-p R007__.. \
+	--bucket="$1" --bucket-type=couchbase \
+	--bucket-ramsize=100 \
+	--bucket-replica=1 \
+	--wait
+
+#-c http://localhost:8091 \
---
+++ couchbase/init-create-scope.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+/opt/couchbase/bin/couchbase-cli collection-manage \
+	-c localhost \
+	-u root \
+	-p R007__.. \
+	--bucket="$1" \
+	--create-scope "$2"
+
+#-c http://localhost:8091 \
